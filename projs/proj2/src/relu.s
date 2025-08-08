c32b34dc89a33868d38b8634b7064df273678629
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-08 17:59:31 +0800

    feat(proj2): complete part-A task-2 - relu

--- projs/proj2/src/relu.s
+++ projs/proj2/src/relu.s
@@ -14,21 +14,19 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    ble a1, x0, malformed # ! length of array less than 1
 
 loop_start:
-
-
-
-
-
-
-
+    lw t0, 0(a0)      # Load current element
+    bge t0, zero, loop_continue # If element >= 0, skip negation
+    sw x0, 0(a0)      # Set the element to 0
 
 loop_continue:
-
-
-
+    addi a0, a0, 4    # Move to the next element
+    addi a1, a1, -1    # Decrement the element count
+    bnez a1, loop_start # If more elements, repeat
+    j loop_end         # Jump to the end of the loop
+    
 loop_end:
 
 
@@ -36,3 +34,7 @@ loop_end:
 
 
     jr ra
+
+malformed: # ! length of array less than 1
+    li a0 36
+    j exit
