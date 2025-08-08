f8d5f0cc2ccfefc68e8775b4097992ee1350356a
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-08 19:50:02 +0800

    feat(proj2): complete part-A task-4 - dot product

--- projs/proj2/src/dot.s
+++ projs/proj2/src/dot.s
@@ -20,20 +20,29 @@
 dot:
 
     # Prologue
+    ble a2, x0, malform_ne # ! length of array less than 1
+    slti t3, a3, 1   # ! stride of arr0 less than 1
+    slti t4, a4, 1   # ! stride of arr1 less than 1
+    or   t0, t3, t4  # ! either is an error
+    bnez t0, malform_st # ! stride malformed
 
+    mv t0, a0        # t0 points to element of arr0
+    mv t1, a1        # t1 points to element of arr1
+    mv t2, a2        # t2 holds the number of elements
+    slli t3, a3, 2   # t3 holds the byte-stride of arr0
+    slli t4, a4, 2   # t4 holds the byte-stride of arr1
+    mv a0, zero      # a0 will hold the dot product result
 
 loop_start:
+    lw a2, 0(t0)     # Load current element of arr0
+    lw a3, 0(t1)     # Load current element of arr1
+    mul a1, a2, a3   # Multiply the elements
+    add a0, a0, a1   # Add to the dot product result
 
-
-
-
-
-
-
-
-
-
-
+    add t0, t0, t3   # Move to the next element in arr0
+    add t1, t1, t4   # Move to the next element in arr1
+    addi t2, t2, -1  # Decrement the element count
+    bnez t2, loop_start # If more elements, repeat
 
 loop_end:
 
@@ -42,3 +51,10 @@ loop_end:
 
 
     jr ra
+
+malform_ne: # ! length of array less than 1
+    li a0 36
+    j exit
+malform_st: # ! stride of array less than 1
+    li a0 37
+    j exit
