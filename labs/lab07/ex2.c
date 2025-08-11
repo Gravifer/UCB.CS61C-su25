204e2a3eaeb2afeef9260a92c6933aec76576b4c
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-11 20:30:20 +0800

    feat(lab07): implement dotp_critical -  exercise 1

--- labs/lab07/ex2.c
+++ labs/lab07/ex2.c
@@ -10,8 +10,20 @@ double dotp_naive(double* x, double* y, int arr_size) {
 // Critical Keyword
 double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
+    // DONE: Implement this function
     // Use the critical keyword here!
+    #pragma omp parallel
+    {
+        double local_sum = 0.0;
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++) {
+            local_sum += x[i] * y[i];
+        }
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
+    }
     return global_sum;
 }
 
